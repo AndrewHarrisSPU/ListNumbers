6f4b501619e53c8905e156a96fe2d982d53b2de0
harrisa15@spu.edu <harrisa15@spu.edu>
2019-06-26 09:45:32 -0700

    binary

--- numbers.cpp
+++ numbers.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-	for( auto i = 0; i < 100; ++i )
+	for( int i = 0; i < 100; ++i ){
 		std::cout << i << ".\n";
+
+		for( int shift = 7; shift >= 0; --shift ){
+			int bit = i;
+			bit = bit >> shift;
+			bit = bit & 0x1;
+			std::cout << bit;
+		}
+
+		std::cout << "\n";
+	}
 }
